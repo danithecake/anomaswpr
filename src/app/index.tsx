3a80c30a370e33299a31488d83e919d21a483a79
Danil Golovachev <danithecake@gmail.com>
2025-03-13 19:44:22 +0300

    chore(package): add smoothscroll-polyfill dependency
    Details: https://caniuse.com/css-scroll-behavior

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import smoothscroll from 'smoothscroll-polyfill'
 import './index.css'
 import App from './App'
+
+smoothscroll.polyfill()
 
 createRoot(document.getElementById('root') as Element).render(
   <StrictMode>
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,5 @@
+declare module 'smoothscroll-polyfill' {
+  function polyfill(): void
+}
+
 declare type FC<P = { [key: string]: unknown }> = React.FunctionComponent<P>
